8390dcb16b9d83142ef73cd55475c14c8bd23d2f
Aref Karimi <arefkr@gmail.com>
2025-03-11 20:56:12 +0000

    Added string functions

---
+++ Calculations/Calculations/Calculations.Test/NamesTest.cs
@@ -0,0 +1,36 @@
+﻿namespace Calculations.Test;
+
+public class NamesTest
+{
+    [Fact]
+    public void MakeFullName_GivenFirstNameLatName_ReturnsFullName()
+    {
+        var names = new Names();
+        var fullName = names.MakeFullName("Aref", "Karimi");
+        Assert.Equal("aref Karimi", fullName , ignoreCase: true);
+    }
+
+    [Fact]
+    public void MakeFullName_GivenFirstNameLatName_MatchesRegex()
+    {
+        var names = new Names();
+        var fullName = names.MakeFullName("Aref", "Karimi");
+        Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+",fullName);
+    }
+
+    [Fact]
+    public void MakeFullName_GivenFirstNameLatName_FirstNameExists()
+    {
+        var names = new Names();
+        var fullName = names.MakeFullName("Aref", "Karimi");
+        Assert.Contains("aref", fullName, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    [Fact]
+    public void MakeFullName_GivenFirstNameLatName_BeginsWithFirstName()
+    {
+        var names = new Names();
+        var fullName = names.MakeFullName("Aref", "Karimi");
+        Assert.StartsWith("Aref" , fullName , StringComparison.InvariantCultureIgnoreCase);
+    }
+}
---
+++ Calculations/Calculations/Calculations/Names.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculations;
+
+public class Names
+{
+    public string MakeFullName(string firstName, string lastName)
+    {
+        return $"{firstName} {lastName}";
+    }
+}
