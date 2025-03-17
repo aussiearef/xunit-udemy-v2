3e6b8e98d1b70bc9d2a03e74546b95788c8eb192
Aref Karimi <arefkr@gmail.com>
2025-03-17 01:26:21 +0000

    Added Collections Tests

--- Calculations/Calculations/Calculations.Test/CalculatorTest.cs
+++ Calculations/Calculations/Calculations.Test/CalculatorTest.cs
@@ -27,4 +27,39 @@ public class CalculatorTest
         // Assert
         Assert.Equal(2.8m , sum , 1);
     }
+
+    [Fact]
+    public void GetFibonacci_DoesNotInclude0()
+    {
+        var calculator = new Calculator();
+        var fibo = calculator.GetFibonacci(5);
+        Assert.All(fibo, n=> Assert.NotEqual(0, n));
+    }
+
+    [Fact]
+    public void GetFibonacci_DoesNotInclude4()
+    {
+        var calculator = new Calculator();
+        var fibo = calculator.GetFibonacci(5);
+        Assert.DoesNotContain(4, fibo);
+    }
+
+    [Fact]
+    public void GetFibonacci_Includes5()
+    {
+        var calculator = new Calculator();
+        var fibo = calculator.GetFibonacci(5);
+        Assert.Contains(5, fibo);
+    }
+
+    [Fact]
+    public void GetFibonacci_First5MembersAreCorrect()
+    {
+        var calculator = new Calculator();
+        var fibo = calculator.GetFibonacci(5);
+        var expectedSeries = new List<int>() { 1,1,2,3,5};
+
+        Assert.Equal(expectedSeries, fibo);
+    }
+
 }
--- Calculations/Calculations/Calculations/Calculator.cs
+++ Calculations/Calculations/Calculations/Calculator.cs
@@ -12,4 +12,22 @@ public class Calculator
         var sum = a + b;
         return Math.Round(sum, 2);
     }
+
+    public IEnumerable<int> GetFibonacci(int length)
+    {
+        
+        var first = 0;
+        var second = 1;
+        for (int i = 0; i < length; i++)
+        {
+            var next = first + second;
+
+            first = second;
+            second = next;
+
+            yield return first;
+        }
+
+        // 1 1 3 5 8 13 ...
+    }
 }
