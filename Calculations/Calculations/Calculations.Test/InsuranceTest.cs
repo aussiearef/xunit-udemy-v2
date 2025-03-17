75a01506f6b82c343618c4d08f3ba787e4275045
Aref Karimi <arefkr@gmail.com>
2025-03-17 09:03:14 +0000

    Added Range and Exception asserts

---
+++ Calculations/Calculations/Calculations.Test/InsuranceTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Calculations.Test;
+
+public class InsuranceTest
+{
+    [Fact]
+    public void DiscountPercentage_GivenAgeOlderThan18_DiscountBetween5And20()
+    {
+        // Arrange
+        var insurance = new Insurance();
+
+        // Act
+        var discount = insurance.DiscountPercentage(70);
+
+        // Assert
+
+        Assert.InRange(discount, 5, 20);
+    }
+
+    [Fact]
+    public void DiscountPercentage_GivenAgeBelow25_DiscountIs5()
+    {
+        //Arrange
+        var insurance = new Insurance();
+
+        // Act
+        var discount = insurance.DiscountPercentage(24);
+
+        // Assert
+        Assert.Equal(5, discount);
+    }
+
+    /*
+     *  Add two or three unit tests for ages between 25 and 70
+     */
+
+    [Fact]
+    public void DiscountPercentage_GivenAgeIsBelow18_ThrowsException()
+    {
+        // Arrange
+        var insurance = new Insurance();
+
+        // Act and Assert
+        Assert.Throws<InvalidDataException>(()=> insurance.DiscountPercentage(5));
+    }
+}
---
+++ Calculations/Calculations/Calculations/Insurance.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace Calculations;
+
+public class Insurance
+{
+    public int DiscountPercentage(int age)
+    {
+        switch (age)
+        {
+            case >= 65: return 5;
+            case >= 45: return 10;
+            case >= 25: return 20;
+            case >= 18: return 5;
+            default: throw new InvalidDataException();
+        }
+    }
+}
