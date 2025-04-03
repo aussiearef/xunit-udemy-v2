9ad5ef2dc504da8a2a19d0f15fd5c1755446ee29
Aref Karimi <arefkr@gmail.com>
2025-04-03 19:13:17 +0000

    Added TDD

---
+++ TestDrivenDevelopment/CalculatorTests/CalculatorTest.cs
@@ -0,0 +1,39 @@
+﻿using Calculations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void Add_GivenTwoInts_ReturnsSum()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add(1, 2);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Add_GivenSecondValueIsZero_ReturnsFirstValue()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+
+            var result = calc.Add(1, 0);
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
